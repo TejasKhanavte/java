a4fc17202d4c2e4260133be027e628c7e2df289e
TEJAS KHANAVATE <tejaskhanavate1@gmail.com>
2026-07-24 18:16:15 +0530

    Add files via upload

---
+++ nwe.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class nwe {
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        System.out.println("enter number 1");
+        int b= sc.nextInt();
+        System.out.println("enter number 2");
+        int sum = a + b;
+        System.out.println("sum of two numbers is: " + sum);
+    }
+}
